790642e0721b14a98a85ead551f6aaaf0d04a120
Yasin <yassine.aithad@educa.jcyl.es>
2025-10-24 18:22:42 +0200

    refactor: remove order.service.ts as part of event-driven architecture cleanup

--- master_desarrollo_ia/modulo_2/event-driven-arch/order.service.ts
+++
@@ -1,8 +0,0 @@
-import { eventBus } from './event-bus';
-import { OrderCreatedEvent } from './src/events/order-created.event';
-
-export function createOrder(orderId: string, userId: string, total: number) {
-  const event: OrderCreatedEvent = { orderId, userId, total };
-  console.log(`Order ${orderId} created.`);
-  eventBus.emit('order.created', event);
-}
