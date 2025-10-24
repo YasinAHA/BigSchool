3c36f6aee7c509fc40faf576076ce0a133fe1052
Yasin <yassine.aithad@educa.jcyl.es>
2025-10-24 19:48:14 +0200

    feat: add initial migration files for Order and Outbox tables

---
+++ master_desarrollo_ia/modulo_2/monolith-to-distributed/prisma/migrations/20250911144528_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "public"."Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Outbox" (
+    "id" TEXT NOT NULL,
+    "event_type" TEXT NOT NULL,
+    "payload" JSONB NOT NULL,
+    "processed" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Outbox_pkey" PRIMARY KEY ("id")
+);
